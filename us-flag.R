a98d7490d61354556edef87cc4b18cb4cdbb87ca
David W. Body <davidbody@bigcreek.com>
2018-05-13 18:58:02 -0500

    Refactoring and switching to purrr

--- us-flag.R
+++ us-flag.R
@@ -1,4 +1,5 @@
 library(grid)
+library(purrr)
 
 scale <- 3
 flag_hoist <- 1.0 * scale
@@ -15,35 +16,37 @@ vp <- viewport(x = 0.5, y = 0.5,
                width = unit(flag_fly, "inches"))
 pushViewport(vp)
 
-stripe <- Vectorize(function(n) {
-    color <- if (n %% 2 == 0) "white" else old_glory_red
-    list(x = 0, y = flag_hoist - n * (flag_hoist / 13),
-         height = flag_hoist / 13, width = flag_fly,
-         color = color)
-}, SIMPLIFY = FALSE)
+make_stripes <- function() {
+    make_a_stripe <- function(n) {
+        color <- if (n %% 2 == 0) "white" else old_glory_red
+        list(x = 0, y = flag_hoist - n * (flag_hoist / 13),
+             height = flag_hoist / 13, width = flag_fly,
+             color = color)
+    }
+    map(1:13, make_a_stripe)
+}
 
-stripes <- stripe(1:13)
-
-drawStripe <- function(s) {
+draw_a_stripe <- function(s) {
     grid.rect(unit(s$x, "inches"), unit(s$y, "inches"),
               unit(s$width, "inches"), unit(s$height, "inches"),
               gp = gpar(fill = s$color, col = s$color),
               just = c("left", "bottom"))
 }
 
-lapply(stripes, drawStripe)
+stripes <- make_stripes()
+walk(stripes, draw_a_stripe)
 
 grid.rect(0, unit(flag_hoist - union_hoist, "inches"),
           unit(union_fly, "inches"), unit(union_hoist, "inches"),
           gp = gpar(fill = old_glory_blue, col = old_glory_blue),
           just = c("left", "bottom"))
 
-star <- function(x, y, radius) {
+make_a_star <- function(x, y, radius) {
     theta <- seq(pi/2, 4*pi, 4*pi/5)
     data.frame(x = x + radius * cos(theta), y = y + radius * sin(theta))
 }
 
-starsForRow <- function(n) {
+stars_for_row <- function(n) {
     if (n %% 2 == 0) {
         xs <- seq(0.126, 5 * 0.126, 0.126) * scale
         ys <- rep(1 - n * 0.054, 5) * scale
@@ -53,19 +56,18 @@ starsForRow <- function(n) {
         ys <- rep(1 - n * 0.054, 6) * scale
         rs <- rep(0.0308, 6) * scale
     }
-    mapply(star, xs, ys, rs, SIMPLIFY = FALSE)
+    pmap(list(xs, ys, rs), make_a_star)
 }
 
-drawStar <- function(s) {
-    grid.polygon(x = s$x, y = s$y,
+draw_a_star <- function(star) {
+    grid.polygon(x = star$x, y = star$y,
                  gp = gpar(col = "white", fill = "white"),
                  default.units = "inches")
 }
 
-star_rows <- c(1:9)
-lapply(star_rows, function(row) {
-    stars <- starsForRow(row)
-    lapply(stars, drawStar)
+walk(1:9, function(row) {
+    stars <- stars_for_row(row)
+    walk(stars, draw_a_star)
 })
 
 grid.rect(gp = gpar(fill = NA))
